7476471aa9b7279d419267ef38104bd6b9413910
Oscar Pacheco <oscarpa_2000@hotmail.com>
2022-04-21 05:59:10 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,48 @@
+import os
+from time import sleep
+
+from flask import render_template, redirect, url_for, Flask, request
+from flask_cors import CORS
+
+from models import report
+
+app = Flask(__name__, static_url_path='',
+            static_folder='static',
+            template_folder='templates')
+CORS(app)
+app.config["DEBUG"] = False
+app.config['host'] = '0.0.0.0'
+app.config['port'] = 5000
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'xlsx'}
+
+report_data = report("0", "0", "Colombia")
+report_data.set_data()
+
+
+@app.route("/")
+@app.route("/home")
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.filename = f.filename.split('.')[1]
+        f.filename = "data." + f.filename
+        filename = (os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
+        f.save(filename)
+        f.close()
+        report_data.set_data()
+        # sleep(5)
+        return redirect(url_for('dashboard'))
+
+
+# Segunda gráfica - Estado
+@app.route("/data2", methods=['GET'])
+def data2():
+    # sleep(1)
+    return report_data.state_of_package().to_json()
---
+++ static/graph2.js
@@ -0,0 +1,43 @@
+/* globals Chart:false, feather:false */
+
+(function () {
+    'use strict'
+
+    feather.replace({ 'aria-hidden': 'true' })
+
+    let ctx = document.getElementById("graph2");
+    $.getJSON('/data2', function(data) {
+        // make a pie chart
+        // print the keys of the data2
+        let keys = Object.keys(data);
+        let values = Object.values(data);
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: keys,
+                datasets: [{
+                    axis: 'x',
+                    label: 'Número de paquetes',
+                    data: values,
+                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 2
+                }]
+            },
+            options: {
+                elements: {
+                    bar: {
+                        borderWidth: 2,
+                    }
+                },
+                responsive: true,
+                indexAxis: 'x',
+                plugins: {
+                    legend: {
+                        position: 'right',
+                    },
+                }
+            },
+        });
+    });
+})()
